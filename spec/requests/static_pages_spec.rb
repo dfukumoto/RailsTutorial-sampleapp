9606d607ec79f774b9e74d37b841539e23c21029
DaiFukumoto <dfukumoto@cuon.co.jp>
2015-04-06 15:47:34 +0900

    テストコードのタイポを修正する．

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,21 +5,8 @@ describe "StaticPages" do
 
 		it "should habe the content 'Sample App'" do
 			visit '/static_pages/home'
-			exepect(page).to have_content('Sample App')
+			expect(page).to have_content('Sample App')
 		end
 
 	end
-end
-
-
-
-
-describe "StaticPages" do
-  describe "/static_pages" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get static_pages_index_path
-      response.status.should be(200)
-    end
-  end
 end
