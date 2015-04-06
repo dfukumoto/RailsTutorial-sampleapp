21e00e1da21a45c49d8ba7a955e513525b2b4ac2
DaiFukumoto <dfukumoto@cuon.co.jp>
2015-04-06 15:58:03 +0900

    Helpページをテストするテストコードを作成する．

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,11 +1,20 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-	describe "Home Page" do
+	describe "Home page" do
 
 		it "should habe the content 'Sample App'" do
 			visit '/static_pages/home'
 			expect(page).to have_content('Sample App')
+		end
+
+	end
+
+	describe "Help page" do
+
+		it "should have the content 'Help'" do
+			visit '/static_pages/help'
+			expect(page).to have_content('Help')
 		end
 
 	end
