9142c4f37525056935fb661b51ee59237085e365
DaiFukumoto <dfukumoto@cuon.co.jp>
2015-04-06 15:42:54 +0900

    /static_pages/homeのHomeページにアクセスしたときに"Sample App"という語が含まれているかをテストするコードを書く．

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,7 +1,21 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-  describe "GET /static_pages" do
+	describe "Home Page" do
+
+		it "should habe the content 'Sample App'" do
+			visit '/static_pages/home'
+			exepect(page).to have_content('Sample App')
+		end
+
+	end
+end
+
+
+
+
+describe "StaticPages" do
+  describe "/static_pages" do
     it "works! (now write some real specs)" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       get static_pages_index_path
