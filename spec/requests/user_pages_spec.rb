b289aab9deef7e1bb624a4cdafd735c9d0b76ffd
DaiFukumoto <dfukumoto@cuon.co.jp>
2015-04-10 18:37:26 +0900

    FactoryGirlを使ってUserモデルのオブジェクトを生成して，Userプロファイルを表示するViewのテストコードを書く．
    * テストを実行するとBCryptのハッシュ生成で時間がかかるので，test環境でのみコスト関数を下げるように設定を変更する．

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -8,4 +8,14 @@ describe "UserPages" do
 		it { should have_content('Sign up') }
 		it { should have_title(full_title('Sign up')) }
 	end
+
+	describe "profile page" do
+		let(:user) { FactoryGirl.create(:user) }
+
+		# ユーザ変数を作成するためのコードに置き換える．
+		before { visit user_path(user) }
+
+		it { should have_content(user.name) }
+		it { should have_title(user.name) }
+	end
 end
