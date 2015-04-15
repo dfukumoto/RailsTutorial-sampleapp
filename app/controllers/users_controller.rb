65f75731d5e3af4cfb5c897dfb21a377f79f3e1b
DaiFukumoto <dfukumoto@cuon.co.jp>
2015-04-15 11:54:45 +0900

    サインインした状態でnewとcreateアクションにアクセスした時，root_pathにリダイレクトする．
    * GETリクエストでUsers#newにアクセスした時，root_pathへリダイレクトする．
    * POSTリクエストでUsers#createにアクセスした時，root_pathへリダイレクトする．

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :signed_in_user,  only: [ :index, :edit, :update, :destroy ]
   before_action :correct_user,    only: [ :edit, :update ]
   before_action :admin_user,      only:   :destroy
+  before_action :signed_in_user_incorrect_access, only: [ :create, :new ]
 
   def index
     @users = User.paginate(page: params[:page]) 
@@ -64,6 +65,12 @@ class UsersController < ApplicationController
 
     def admin_user
       redirect_to(root_path) unless current_user.admin?
+    end
+
+    def signed_in_user_incorrect_access
+      if signed_in?
+        redirect_to(root_path)
+      end
     end
 
 end
